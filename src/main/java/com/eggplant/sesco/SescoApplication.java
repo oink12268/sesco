b452fe0ad71a5cb23c6f352c00d85f2abf406568
oink12268@gmail.com <shkim@smartscore.kr>
2021-12-07 15:13:16 +0900

    first commit

---
+++ src/main/java/com/eggplant/sesco/SescoApplication.java
@@ -0,0 +1,13 @@
+package com.eggplant.sesco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SescoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SescoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eggplant/sesco/SescoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eggplant.sesco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SescoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
