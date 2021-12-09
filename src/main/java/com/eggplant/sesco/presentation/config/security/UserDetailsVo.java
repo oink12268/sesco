f547641f17fe5859b4a3c7540794ca859486bcc9
oink12268@gmail.com <shkim@smartscore.kr>
2021-12-09 10:54:45 +0900

    commit test

--- src/main/java/com/eggplant/sesco/presentation/config/security/UserDetailsVo.java
+++ src/main/java/com/eggplant/sesco/presentation/config/security/UserDetailsVo.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 public class UserDetailsVo implements UserDetails {
 
     private String username;
-    private Collection<? extends  GrantedAuthority> authorities; // test123
+    private Collection<? extends  GrantedAuthority> authorities; // test1234
 
     @Override
     public String getUsername() {
